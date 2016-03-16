80b339e2de76d1caea576c1ea058e9a95a822760
Lars Holmberg <K3LARRA@HOTMAIL.COM>
2016-03-16 13:00:30 +0100

    One

---
+++ Example 1/src/MyOther.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class MyOther {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Item> al = new ArrayList();
+
+	}
+
+}
---
+++ Example 2/src/Fidus.java
@@ -0,0 +1,52 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JLabel;
+
+public class Fidus extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Fidus frame = new Fidus();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Fidus() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.WEST);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		scrollPane.setColumnHeaderView(lblNewLabel);
+
+	}
+
+}
